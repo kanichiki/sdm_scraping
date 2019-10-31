10f50ab4d8a13d99b7cce76bc3ccbbe5635a5876
k-ichiki <ichiki@fujisan.co.jp>
2019-10-31 18:47:33 +0900

    modified main.py

--- main.py
+++ main.py
@@ -58,9 +58,10 @@ def handle_message(event):
     #     event.reply_token,
     #     TextSendMessage(text=event.message.text))
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=test_message))
+    if(event.message.text=="うんこ"):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=test_message))
 
 
 if __name__ == "__main__":
