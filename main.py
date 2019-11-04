76ae4230b1407bb3b6afcd0f58db42e9131e2131
k-ichiki <ichiki@fujisan.co.jp>
2019-11-04 17:55:32 +0900

    modified main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def handle_message(event):
                   {
                     "type": "text",
                     "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                    "wrap": true,
+                    "wrap": True,
                     "color": "#666666",
                     "size": "sm",
                     "flex": 5
@@ -115,7 +115,7 @@ def handle_message(event):
                   {
                     "type": "text",
                     "text": "10:00 - 23:00",
-                    "wrap": true,
+                    "wrap": True,
                     "color": "#666666",
                     "size": "sm",
                     "flex": 5
