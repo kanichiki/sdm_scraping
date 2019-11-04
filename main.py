df1166421e317f21d8211429bb95914a8bdb17e1
k-ichiki <ichiki@fujisan.co.jp>
2019-11-04 17:51:14 +0900

    modified main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
+    FlexSendMessage, MessageEvent, TextMessage, TextSendMessage
 )
 import os
 import scraping
